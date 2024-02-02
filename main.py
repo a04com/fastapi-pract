b71312de1776da774f77b25c79e5341853e36b9b
Ali <alasylkhh@gmail.com>
2024-02-02 12:59:01 +0600

    queries

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-
+from weather import getForecast
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello"}
 
+@app.get("/weather")
+async def weather(q: str, days: int):
+    return getForecast(q, days)
